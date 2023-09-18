44db77749b990a53e736c678467510e712741028
omkar <omkar@OMKAR_SOCKOSOFT>
2023-09-18 17:39:30 +0530

    Add project files.

---
+++ Controllers/Api/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NoteKeeper.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Content("api started successfully");
+        }
+    }
+}
