3fb604e4f0153859dfc6377aea220c9c14b65f0a
ukirdeomkar <ukirdeomkar4@gmail.com>
2023-09-23 00:26:03 +0530

    shareNote Model Added

---
+++ Controllers/Api/ShareNoteController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NoteKeeper.Models;
+
+namespace NoteKeeper.Controllers.Api
+{
+    [ApiController]
+    [Route("notekeeper/[controller]")]
+    public class ShareNoteController : Controller
+    {
+        //private MyDBContext _context;
+        //private readonly IMapper _mapper;
+        //public ShareNoteController(IMapper mapper, MyDBContext my_context)
+        //{
+        //    _context = my_context;
+        //    _mapper = mapper;
+        //}
+        public ShareNoteController()
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            return Ok("Sharing is On");
+        }
+
+
+        [HttpPost("share/{noteId}")]
+        public IActionResult SharingNote(int noteId, ShareNote sharedNote)
+        {
+            // Validate request and permissions, generate unique link, and store it in the database
+            // Return the unique link to the user
+            return Ok(new { link = "link" });
+        }
+    }
+}
---
+++ Models/ShareNote.cs
@@ -0,0 +1,18 @@
+﻿namespace NoteKeeper.Models
+{
+    public class ShareNote
+    {
+        public int Id { get; set; }
+        public int NoteId { get; set; }
+        public string UniqueLink { get; set; }
+        public string Permission { get; set; } // "view", "edit", "delete"
+
+        public Note? Note { get; set; }
+
+        public static readonly int notShared = 0;
+        public static readonly int viewNote = 1;
+        public static readonly int editNote = 2;
+        public static readonly int deleteNote = 3;
+
+    }
+}
